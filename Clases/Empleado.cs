f6be83e433d768cf91f448ef9464b14d651f2505
Wilmer Geraldo Reyes <geraldowilmer75@gmail.com>
2024-03-02 19:57:48 -0400

    Agregar archivos de proyecto.

---
+++ Clases/Empleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2do_Parcial_LP2.Clases
+{
+    internal class Empleado
+    {
+        private int idEmpleado {  get; set; }
+        private string CodigoEmpleado { get; set; } = null!;
+        private string Cedula { get; set; } = null!;
+        private string Nombre { get; set; } = null!;
+        private string Apellido { get; set; } = null!;
+        private string Cargo { get; set; } = null!;
+        private string Departamento { get; set; } = null!;
+        private DateTime FechaInicioEmpleo { get; set; } = new DateTime(0000, 0, 0);
+        private string CargoEmpleado { get; set; } = null!;
+        private string Email { get; set; } = null!;
+        private string Direccion { get; set; } = null!;
+        private string Telefono { get; set; } = null!;
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace _2do_Parcial_LP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
