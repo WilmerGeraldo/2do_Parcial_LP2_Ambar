44ead7e1d2df53a01bc701c4d7344faeb35f043f
Wilmer Geraldo Reyes <geraldowilmer75@gmail.com>
2024-03-02 22:44:28 -0400

    Clase User Add a DB by Wilmer

---
+++ Clases/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2do_Parcial_LP2.Clases
+{
+    internal class Usuario
+    {
+        private int idUsuario { get; set; }
+        private string Nombre { get; set; } = null!;
+        private string Clave {  get; set; } = null!;
+        private string Cargo { get; set; } = null!;
+    }
+}
