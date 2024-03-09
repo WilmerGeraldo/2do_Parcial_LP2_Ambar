685fed81f534016bab8f19bc1c41314fc3a8baf8
Wilmer Geraldo Reyes <geraldowilmer75@gmail.com>
2024-03-09 19:44:25 -0400

    Que joder con esta vaina 😒😒

--- obj/Release/net8.0-windows/2do_Parcial_LP2.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/2do_Parcial_LP2.AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("2do_Parcial_LP2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+193b74c9d19ac2d3a2848e57d95154ecf0905264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df54a764dcfe8224b0d88ad7e8abff1a3a31b54")]
 [assembly: System.Reflection.AssemblyProductAttribute("2do_Parcial_LP2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2do_Parcial_LP2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
