9fc5a017eb4c4e35fee3c9253fb0d86826edea88
Wilmer Geraldo Reyes <geraldowilmer75@gmail.com>
2024-03-09 00:26:12 -0400

    Cambios

--- obj/Debug/net8.0-windows/2do_Parcial_LP2.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/2do_Parcial_LP2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2do_Parcial_LP2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e16e408656f57c04950a030bcb13fdd1244335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f88abd197ff0e009d29b9bf797bf88ac46da5e64")]
 [assembly: System.Reflection.AssemblyProductAttribute("2do_Parcial_LP2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2do_Parcial_LP2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
